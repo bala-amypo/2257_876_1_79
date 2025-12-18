b914b69bc85e8ac02c304a3b82b0540d9f07418e
bala-amypo <bala@amypo.in>
2025-12-18 08:23:04 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,19 +1,12 @@
 package com.example.demo.entity;
-
-
 import jakarta.persistence.*;
-
-
 @Entity
-@Table(name = "users", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
 public class User {
-
-
 @Id
 private Long id;
 private String name;
-@Column(nullable = false, unique = true)
+@Column( unique = true)
 private String email;
-
-
 private String password;
+private String role;
+}
