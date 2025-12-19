49312495594ee8b3dfa244b732514ec4ebb110c7
bala-amypo <bala@amypo.in>
2025-12-19 08:01:23 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,36 +1,16 @@
 package com.example.demo.entity;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.*;
-import java.util.List;
 import lombok.*;
 @Entity
-@Table(
-    name = "users",
-    uniqueConstraints = @UniqueConstraint(columnNames = "email")
-)
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotBlank
-    @Size(min = 2, max = 100)
-    private String name;
-    @Email
-    @NotBlank
-    @Column(nullable = false, unique = true)
+    @Column(unique = true, nullable = false)
     private String email;
-    @NotBlank
-    @Size(min = 6)
     private String password;
-    @NotBlank
     private String role;
-    @OneToMany(
-        mappedBy = "user",
-        cascade = { CascadeType.PERSIST, CascadeType.MERGE }
-    )
-    private List<Vehicle> vehicles;
 }
