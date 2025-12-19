19fcf87f25fcb5ebe736a2382442d18e4638e6b1
bala-amypo <bala@amypo.in>
2025-12-19 05:20:13 +0000

    User commit

--- src/main/java/com/example/demo/entity/Vehicle.java
+++ src/main/java/com/example/demo/entity/Vehicle.java
@@ -1,52 +1,30 @@
 package com.example.demo.entity;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import java.util.List;
+import lombok.*;
 @Entity
+@Table(name = "vehicles", uniqueConstraints = @UniqueConstraint(columnNames = "vehicleNumber"))
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Vehicle {
-    @Id
-    private Long id;
-    @ManyToOne
-    private User user;
-    @Column(unique = true)
-    private String vehicleNumber;
-    private Double capacityKg;
-    private Double fuelEfficiency;
-        public Vehicle() {
-    }
-    public Vehicle(User user, String vehicleNumber, Double capacityKg, Double fuelEfficiency){
-        this.user = user;
-        this.vehicleNumber = vehicleNumber;
-        this.capacityKg = capacityKg;
-        this.fuelEfficiency = fuelEfficiency;
-    }
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public User getUser() {
-        return user;
-    }
-    public void setUser(User user) {
-        this.user = user;
-    }
-    public String getVehicleNumber() {
-        return vehicleNumber;
-    }
-    public void setVehicleNumber(String vehicleNumber){
-        this.vehicleNumber = vehicleNumber;
-    }
-    public Double getCapacityKg(){
-        return capacityKg;
-    }
-    public void setCapacityKg(Double capacityKg){
-        this.capacityKg = capacityKg;
-    }
-    public Double getFuelEfficiency(){
-        return fuelEfficiency;
-    }
-    public void setFuelEfficiency(Double fuelEfficiency){
-        this.fuelEfficiency = fuelEfficiency;
-    }
-
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
+@ManyToOne(fetch = FetchType.LAZY, optional = false)
+@JoinColumn(name = "user_id")
+private User user;
+@NotBlank
+@Size(min = 2, max = 50)
+private String vehicleNumber;
+@NotNull
+@Positive
+private Double capacityKg;
+@NotNull
+@Positive
+private Double fuelEfficiency;
+@OneToMany(mappedBy = "vehicle", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+private List<Shipment> shipments;
 }
