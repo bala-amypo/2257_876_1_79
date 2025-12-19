a0319a18b850e86b81450b2ff66ea680e57c0664
bala-amypo <bala@amypo.in>
2025-12-19 05:26:13 +0000

    User commit

--- src/main/java/com/example/demo/entity/Shipment.java
+++ src/main/java/com/example/demo/entity/Shipment.java
@@ -1,62 +1,19 @@
 package com.example.demo.entity;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 import java.time.LocalDate;
-@Entity
-public class Shipment {
-    @Id
-    private Long id;
-    @ManyToOne
-    private Vehicle vehicle;
-    @ManyToOne
-    private Location pickupLocation;
-    @ManyToOne
-    private Location dropLocation;
-    private Double weightKg;
-    private LocalDate scheduledDate;
-     public Shipment() {
-    }
-    public Shipment(Vehicle vehicle,  Location pickupLocation, Location dropLocation, Double weightKg,LocalDate scheduledDate) {
-        this.vehicle = vehicle;
-        this.pickupLocation = pickupLocation;
-        this.dropLocation = dropLocation;
-        this.weightKg = weightKg;
-        this.scheduledDate = scheduledDate;
-    }
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public Vehicle getVehicle() {
-        return vehicle;
-    }
-    public void setVehicle(Vehicle vehicle) {
-        this.vehicle = vehicle;
-    }
-    public Location getPickupLocation() {
-        return pickupLocation;
-    }
-    public void setPickupLocation(Location pickupLocation) {
-        this.pickupLocation = pickupLocation;
-    }
-    public Location getDropLocation() {
-        return dropLocation;
-    }
-    public void setDropLocation(Location dropLocation) {
-        this.dropLocation = dropLocation;
-    }
-    public Double getWeightKg() {
-        return weightKg;
-    }
-    public void setWeightKg(Double weightKg) {
-        this.weightKg = weightKg;
-    }
-    public LocalDate getScheduledDate() {
-        return scheduledDate;
-    }
-    public void setScheduledDate(LocalDate scheduledDate) {
-        this.scheduledDate = scheduledDate;
-    }
-   
-}
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
