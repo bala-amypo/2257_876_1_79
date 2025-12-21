7ac0a0b8bc99637f3bc5ac594329d6f5775c6426
bala-amypo <bala@amypo.in>
2025-12-21 08:53:59 +0000

    User commit

--- src/main/java/com/example/demo/controller/LocationController.java
+++ src/main/java/com/example/demo/controller/LocationController.java
@@ -1,17 +1,12 @@
 package com.example.demo.controller;
-
 import com.example.demo.entity.Location;
 import com.example.demo.service.LocationService;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-
 @RestController
 @RequestMapping("/locations")
 public class LocationController {
-
     private final LocationService locationService;
-
     public LocationController(LocationService locationService) {
         this.locationService = locationService;
     }
