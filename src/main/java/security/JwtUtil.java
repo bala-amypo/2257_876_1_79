ab53f9570681b1e38ce53794f0f3eaeccef84bfa
bala-amypo <bala@amypo.in>
2025-12-22 00:56:26 +0000

    User commit

--- src/main/java/security/JwtUtil.java
+++ src/main/java/security/JwtUtil.java
@@ -1,28 +1,32 @@
 package com.example.demo.security;
-import io.jsonwebtoken.*;
-import io.jsonwebtoken.security.Keys;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Component;
-import java.security.Key;
+
 import java.util.Date;
+
 @Component
 public class JwtUtil {
-    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
-    private final long expirationMillis = 24 * 60 * 60 * 1000;
+
+    private final String SECRET_KEY = "secret123";
+
     public String generateToken(Long userId, String email, String role) {
         return Jwts.builder()
                 .claim("userId", userId)
                 .claim("email", email)
                 .claim("role", role)
                 .setSubject(email)
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + expirationMillis))
-                .signWith(key)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
                 .compact();
     }
-    public Claims validateToken(String token) {
-        return Jwts.parserBuilder()
-                .setSigningKey(key)
-                .build()
+
+    public Claims extractClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
                 .parseClaimsJws(token)
                 .getBody();
     }
