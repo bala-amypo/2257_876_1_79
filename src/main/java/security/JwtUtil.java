afe93740e0c18a59f31fc238641d026713b7d936
bala-amypo <bala@amypo.in>
2025-12-22 00:47:01 +0000

    User commit

--- src/main/java/security/JwtUtil.java
+++ src/main/java/security/JwtUtil.java
@@ -27,3 +27,18 @@ public class JwtUtil {
                 .getBody();
     }
 }
+DemoApplication
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
