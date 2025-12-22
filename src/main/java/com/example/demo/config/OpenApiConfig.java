137c0ae383e073a7eeed26b90b1d7c03b6947ef4
bala-amypo <bala@amypo.in>
2025-12-22 07:42:10 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -13,7 +13,7 @@ public class OpenApiConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .servers(List.of(
-                        new Server().url("https://9111.pro604cr.amypo.ai/")
+                        new Server().url("https://9059.pro604cr.amypo.ai/")
                 ));
         }
 }
