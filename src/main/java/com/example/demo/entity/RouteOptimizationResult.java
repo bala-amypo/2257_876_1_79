4689799007d00485b6a29ea09fd8f5203d1cc917
bala-amypo <bala@amypo.in>
2025-12-24 17:06:47 +0000

    User commit

--- src/main/java/com/example/demo/entity/RouteOptimizationResult.java
+++ src/main/java/com/example/demo/entity/RouteOptimizationResult.java
@@ -1,13 +1,32 @@
 package com.example.demo.entity;
 
-import lombok.Builder;
-import lombok.Data;
+import jakarta.persistence.*;
+import lombok.*;
 
-@Data
+@Entity
+@Table(name = "route_optimization_results")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class RouteOptimizationResult {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // Distance in km
     private double distance;
+
+    // Fuel consumed in liters
     private double fuelUsed;
+
+    // Total cost
     private double totalCost;
+
+    // Optional relation to Shipment
+    @ManyToOne
+    @JoinColumn(name = "shipment_id")
+    private Shipment shipment;
 }
